129a07bfb832cdd3f2b95bcc7b584a940911c7ac
Bonnie Dowling <bonniedowling@comcast.net>
2013-09-15 14:50:17 -0400

    Added my name to index page text

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     	<div class="textbox">
         	<div class="text">
             	<h2>About Me</h2>
-                <p> In 2002 I received my associates degree in visual communications (web/graphic art), yet then began working in a job that hardly utilized my skills. After not keeping up with the field for years I decided to go back to school and began classes at the extension school.  Over the last few years I have taken classes on Java, HTML 5, CSS, and Flash.  I am very excited to be taking this class; I always have ideas for creating dynamic sites, yet don't know where/how to start.  I try to hide it but at heart I am just a big geek.... Yet I do have some other hobbies that don't involve the computer, for example I enjoy arts/crafts, sewing, baking, painting, etc.  Basically, I am a creative person who loves to create things online and off. 
+                <p> My name is Bonnie Dowling, in 2002 I received my associates degree in visual communications (web/graphic art), yet then began working in a job that hardly utilized my skills. After not keeping up with the field for years I decided to go back to school and began classes at the extension school.  Over the last few years I have taken classes on Java, HTML 5, CSS, and Flash.  I am very excited to be taking this class; I always have ideas for creating dynamic sites, yet don't know where/how to start.  I try to hide it but at heart I am just a big geek.... Yet I do have some other hobbies that don't involve the computer, for example I enjoy arts/crafts, sewing, baking, painting, etc.  Basically, I am a creative person who loves to create things online and off. 
                 </p>  
             	<p>For a code editor I use TextMate and sometimes Dreamweaver</p>
 			</div>
