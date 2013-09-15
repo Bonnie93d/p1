2fe81b55d6edaedc935bb0f5987e5b5786455656
Bonnie Dowling <bonniedowling@comcast.net>
2013-09-15 13:53:01 -0400

    Adding first index page

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Home</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+ 
+<body>
+
+	<header>
+      <div class="HeaderImage"> 
+			<nav>
+				<!--future nav menu to be inserted here -->
+            </nav>
+      </div>
+	</header>
+   
+  
+	<div class="picArt">
+    	<img class="background" src="Assets/topImage.jpg" alt="" title="" />
+	</div>
+
+	<article>
+    	<div class="textbox">
+        	<div class="text">
+            	<h2>About Me</h2>
+                <p> In 2002 I received my associates degree in visual communications (web/graphic art), yet then began working in a job that hardly utilized my skills. After not keeping up with the field for years I decided to go back to school and began classes at the extension school.  Over the last few years I have taken classes on Java, HTML 5, CSS, and Flash.  I am very excited to be taking this class; I always have ideas for creating dynamic sites, yet don't know where/how to start.  I try to hide it but at heart I am just a big geek.... Yet I do have some other hobbies that don't involve the computer, for example I enjoy arts/crafts, sewing, baking, painting, etc.  Basically, I am a creative person who loves to create things online and off. 
+                </p>  
+            	<p>For a code editor I use TextMate and sometimes Dreamweaver</p>
+			</div>
+            <p></p>
+            <p></p>
+		</div>
+	</article>	
+
+
+	<footer>
+    	<address>
+        123 Main Street, Anywhere, <abbr title="Massachusetts">MA</abbr> 01234 USA (987) 654-3210
+        </address>
+
+         <p>© 2013 BonnieDowling.com</p>
+	</footer>
+
+</body>
+</html>
+ 
